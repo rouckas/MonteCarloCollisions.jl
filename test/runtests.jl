6a182cd35b1f033c59a183151942ef36777686e5
Stepan Roucka <stepan@roucka.eu>
2021-08-27 16:15:31 +0200

    Add some tests
    just for testing the testing :)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using MonteCarloCollisions
+using Statistics
+using LinearAlgebra
 using Test
 
 @testset "MonteCarloCollisions.jl" begin
+    nsampl = 1000
+    samples = [MonteCarloCollisions.random_direction(1.) for i in 1:nsampl]
+    @test max(norm.(samples)...) ≈ min(norm.(samples)...)
+    @test mean(samples) < std(samples)/sqrt(nsampl)*5
     # Write your tests here.
 end
