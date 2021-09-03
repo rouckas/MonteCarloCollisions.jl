d51327138891fa07e5d99db9c8a9b19a86aa6e0e
PanKanec <79411686+PanKanec@users.noreply.github.com>
2021-09-03 13:47:29 +0200

    test_commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,3 +10,38 @@ using Test
     @test mean(samples) < std(samples)/sqrt(nsampl)*5
     # Write your tests here.
 end
+
+@testset "negative integers - test" begin
+    nsampl = 1000
+    random_num_array = [ran(r[-1:-10000]) for n in 1:nsampl]
+    for x in random_num_array
+        variable = random_maxwell_v(x)
+        @test variable .< 0
+    end
+end
+
+@testset "particle_test" begin
+
+    negative_weight_particle = MonteCarloCollisions.Particles("negative_weight", -1, 100, 1)
+
+    reference_particle = MonteCarloCollisions.Particles("reference", 4, 100, 1)
+
+    huge_weight_particle = MonteCarloCollisions.Particles("huge_weight", 5000, 100, 1)
+
+    MonteCarloCollisions.init_monoenergetic(negative_weight_particle.list[1])
+
+    MonteCarloCollisions.init_monoenergetic(reference_particle.list[1])
+
+    MonteCarloCollisions.init_monoenergetic(huge_weight_particle.list[1])
+
+    init_thermal(negative_weight_particle)
+    @test negative_weight_particle.list[1].t = 0
+
+    @test energy(negative_weight_particle) < energy(reference_particle) < energy(huge_weight_particle)
+
+end
+
+
+    
+
+    
