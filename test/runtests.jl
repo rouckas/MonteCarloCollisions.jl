654859ca63774f43ddac455f6fc40eee45ab35e1
PanKanec <79411686+PanKanec@users.noreply.github.com>
2021-09-09 23:00:17 +0200

    test_01

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,58 +13,28 @@ end
 
 @testset "particle_tests" begin
 
-    negative_weight_particle = MonteCarloCollisions.Particles("negative_weight", -1, 100, 1)
+        oxygen_test_particle = MonteCarloCollisions.Particles("oxygen_test", 16*amu, q_e, 2)
+    
+        fluor_test_particle = MonteCarloCollisions.Particles("Fluor", 19*amu, q_e, 2)
+    
+        sodium_test_particle = MonteCarloCollisions.Particles("sodium_test_particle", 23*amu, q_e, 2)
+    
+        MonteCarloCollisions.init_thermal(sodium_test_particle, 10)
+    
+        MonteCarloCollisions.init_thermal(fluor_test_particle, 10)
+    
+        MonteCarloCollisions.init_thermal(oxygen_test_particle, 10)
+    
+        scatter_v = MonteCarloCollisions.scatter(sodium_test_particle.list[1].v, fluor_test_particle.list[1].v, sodium_test_particle.m, fluor_test_particle.m, norm(sodium_test_particle.list[1].v - fluor_test_particle.list[1].v), 0)
+            
+        @test fluor_test_particle.list[1].v != scatter_v
 
-    reference_particle = MonteCarloCollisions.Particles("reference", 4, 100, 1)
+        @test energy(sodium_test_particle) > energy(fluor_test_particle) > energy(oxygen_test_particle)
 
-    huge_weight_particle = MonteCarloCollisions.Particles("huge_weight", 5000, 100, 1)
-
-    MonteCarloCollisions.init_monoenergetic(reference_particle, 10)
-
-    MonteCarloCollisions.init_monoenergetic(huge_weight_particle, 10)
-
-    scatter_v = MonteCarloCollisions.scatter(reference_particle.list[1].v, huge_weight_particle.list[1].v, reference_particle.m, huge_weight_particle.m, norm(reference_particle.list[1].v - huge_weight_particle.list[1].v), 0)
-
-    electrons.list[1].t = 8
-
-    init_time(negative_weight_particle)
-
-    @test negative_weight_particle.list[1].t = 0
-
-    @test energy(negative_weight_particle) < energy(reference_particle) < energy(huge_weight_particle)
-
-    @test norm(reference_particle.list[1].v) != norm(scatter_v)
+        @test norm(reference_particle.list[1].v) != norm(scatter_v)
 
 end
 
-@testset "maxwell_tests" begin 
-
-    electrons = MonteCarloCollisions.Particles("electron", m_e, q_e, 10000);
-
-    MonteCarloCollisions.init_thermal(electrons, 100.)
-
-    maxwell_test_list_electrons = []
-
-    for particle in electrons.list
-        push!(maxwell_test_list, norm(particle.v))
-    end
-    
-    oxygen_test_list = []
-
-
-
-    while length(oxygen_test_list) != 10000
-        g = random_maxwell_v(oxygen_test.vthermal)
-        push!(oxygen_test_list, norm(g))
-    end
-    
-    @test mean(pxygen_test_list) ≈ 6328.
-
-
-    @test mean(maxwell_test_list) ≈ 16200.
-
-
-end
     
 @testset "interaction_tests" begin
 
@@ -74,9 +44,9 @@ end
 
     test_interaction = MonteCarloCollisions.make_interactions(electrons, oxygen_list)
 
-    @test test_intearction.list[1][1].name == "EFFECTIVE O"
+    @test test_interaction.list[1][1].name == "EFFECTIVE O"
 
-    @test lenght(test_interaction.spec2_names) == 1 
+    @test length(test_interaction.spec2_names) == 1 
     
     @test typeof(test_interaction.spec2_list[1]) == Neutrals
 
@@ -86,14 +56,16 @@ end
 
     @test length(test_interaction.list) == 1
 
-    MonteCarloCollisions.svmax_find!(test_interaction, 20.)
+    svmax_find!(test_interaction, 20.)
 
-    MonteCarloCollisions.init_rates!(test_intearction)
+    init_rates!(test_interaction)
 
-    @test length(test_intearction.svmax_list) == 1
+    @test length(test_interaction.svmax_list) == 1
 
     @test test_interaction.svmax_list[1] == 4.930053435374193e-13
 
     @test test_interaction.prob_list[1] == 1.0
 
 end
+
+
