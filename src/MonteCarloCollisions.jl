cfa8cc79947b1d74bcb5e82c9c76f0d157e40cc7
Stepan Roucka <stepan@roucka.eu>
2023-10-19 23:10:20 +0200

    Move random sampling functions to separate file

--- src/MonteCarloCollisions.jl
+++ src/MonteCarloCollisions.jl
@@ -21,35 +21,7 @@ const amu = ustrip(u"kg", CODATA2018.AtomicMassConstant)
 const q_e = ustrip(u"C", CODATA2018.e)
 const k_B = ustrip(u"J/K", CODATA2018.k_B)
 
-@inline function random_direction(l=1.)
-    """generate a 3D vector with random direction and length l"""
-    cos_theta =  1. - 2*rand()
-    sin_theta = sqrt(1. - cos_theta^2)
-    sin_cos_phi = sincos(2*pi*rand())
-    SVector(
-        l*cos_theta,
-        l*sin_theta * sin_cos_phi[1],
-        l*sin_theta * sin_cos_phi[2]
-    )
-end
-
-@inline function random_maxwell_v(vthermal)
-    SVector(randn(),randn(), randn())*vthermal # sqrt(k_B*T/m)
-end
-
-@inline function choose(probs)
-    # equivalent to dist=Categorical(probs); rand(dist),
-    # > 30% faster, but no error checking (sum(probs) == 1 ?)
-    l = length(probs)
-    i = 1
-    if l > 1
-        U = rand()
-        @inbounds while (U -= probs[i]) > 0 && i < l
-            i += 1
-        end
-    end
-    i
-end
+include("RandomSampling.jl")
 
 abstract type Species end
 
