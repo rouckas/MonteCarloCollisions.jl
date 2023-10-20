93db65ae65db302efd88f12893bd4ff3723f772a
Stepan Roucka <stepan@roucka.eu>
2023-10-20 10:00:50 +0200

    Add tests for rate calculation and thermal dist.

--- test/runtests.jl
+++ test/runtests.jl
@@ -59,7 +59,30 @@ using Test
     argon = NeutralEnsemble("argon", 300., 40*amu, 1e22)
     helium = NeutralEnsemble("helium", 300., 4*amu, 1e23)
     electrons = ParticleEnsemble("electron", m_e, q_e, 1000)
+    argonplus = ParticleEnsemble("argonplus", 40*amu, -q_e, 1000)
     helium_interaction_list = load_interactions_lxcat("../data/CS_e_He.txt", electrons, helium)
+    argon_interaction_list = load_interactions_lxcat("../data/CS_e_Ar.txt", electrons, argon)
+    argonplus_argon_interaction_list = load_interactions_lxcat("../data/CS_e_Ar.txt", argonplus, argon)
     @test length(helium_interaction_list) == 3
     @test length(helium_interaction_list[1].sigmav) == 8
+
+    # test svmax find and rate calculation
+    electron_interactions = make_interactions(electrons, vcat(helium_interaction_list, argon_interaction_list), 20.)
+    argonplus_interactions = make_interactions(argonplus, vcat(argonplus_argon_interaction_list), 1.0)
+    @test (electron_interactions.svmax_list[1] ≈ 8.853142893515225e-14) || (electron_interactions.svmax_list[2] ≈ 8.853142893515225e-14)
+    @test electron_interactions.rate ≈ 1.211705420664051e10
+    @test argonplus_interactions.svmax_list[1] ≈ 4.2865680860459716e-17
+    @test argonplus_interactions.rate ≈ 428656.8086045972
+
+    # test thermal initialization
+    Te = 10000.
+    Ti = 300.
+    init_thermal(electrons, Te)
+    init_thermal(argonplus, Ti)
+
+    using MonteCarloCollisions: k_B
+    @test 0.5*3/2*k_B*Te < mean(energy(electrons)) < 2.0*3/2*k_B*Te
+    @test 0.5*3/2*k_B*Ti < mean(energy(argonplus)) < 2.0*3/2*k_B*Ti
+
+
 end
